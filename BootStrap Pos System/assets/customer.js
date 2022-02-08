81edc6508ed402dc975cc6c7f0399ed408740d15
Mish880 <Pereraittec6@gmail.com>
2022-02-08 16:18:14 +0530

    Update BootStrap

--- BootStrap Pos System/assets/customer.js
+++ BootStrap Pos System/assets/customer.js
@@ -3,6 +3,9 @@
 /*Button Selection*/
 $("#btnSaveCustomer").click(function () {
 
+    /*remove all the row click events added before*/
+    $("#cusTable>tr").off("click");
+
     /*gather the customer information*/
     let customerID = $("#txtCusID").val();
     let customerName = $("#txtName").val();
@@ -17,6 +20,24 @@ $("#btnSaveCustomer").click(function () {
 
     /*bind the event after the row*/
     $("#cusTable>tr").click(function () {
-       console.log(this);
+      /*this*/ /*dom object*/
+      /*$(this); This is a JQuery Object*/
+
+      /*$(this)*/ /*tr*/
+      /*$(this).children();*/ /*Return all td inside selected row*/
+
+      let cusID = $(this).children(":eq(0)").text(); /*Select the first td and get the text*/
+      let cusName = $(this).children(":eq(1)").text();
+      let cusAddress = $(this).children(":eq(2)").text();
+      let cusContact = $(this).children(":eq(3)").text();
+
+      console.log(cusID,cusName,cusAddress,cusContact);
+
+      /*Set values to the input fields*/
+
+        $("#txtCusID").val(cusID);
+        $("#txtName").val(cusName);
+        $("#txtAddress").val(cusAddress);
+        $("#txtContact").val(cusContact);
     });
 });
