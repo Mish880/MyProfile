11209c63484c312982613b9a6db49a13dd5b1c8c
Mish880 <Pereraittec6@gmail.com>
2022-02-13 16:58:11 +0530

    Update BootStrap

--- BootStrap Pos System/assets/customer.js
+++ BootStrap Pos System/assets/customer.js
@@ -20,24 +20,60 @@ $("#btnSaveCustomer").click(function () {
 
     /*bind the event after the row*/
     $("#cusTable>tr").click(function () {
-      /*this*/ /*dom object*/
-      /*$(this); This is a JQuery Object*/
+        /*this*/ /*dom object*/
+        /*$(this); This is a JQuery Object*/
 
-      /*$(this)*/ /*tr*/
-      /*$(this).children();*/ /*Return all td inside selected row*/
+        /*$(this)*/ /*tr*/
+        /*$(this).children();*/ /*Return all td inside selected row*/
 
-      let cusID = $(this).children(":eq(0)").text(); /*Select the first td and get the text*/
-      let cusName = $(this).children(":eq(1)").text();
-      let cusAddress = $(this).children(":eq(2)").text();
-      let cusContact = $(this).children(":eq(3)").text();
+        let cusID = $(this).children(":eq(0)").text(); /*Select the first td and get the text*/
+        let cusName = $(this).children(":eq(1)").text();
+        let cusAddress = $(this).children(":eq(2)").text();
+        let cusContact = $(this).children(":eq(3)").text();
 
-      console.log(cusID,cusName,cusAddress,cusContact);
+        console.log(cusID, cusName, cusAddress, cusContact);
 
-      /*Set values to the input fields*/
+        /*Set values to the input fields*/
 
         $("#txtCusID").val(cusID);
         $("#txtName").val(cusName);
         $("#txtAddress").val(cusAddress);
         $("#txtContact").val(cusContact);
     });
+
+
+    /*Remove selected row form the double click*/
+    $("#cusTable>tr").dblclick(function () {
+        $(this).remove();
+    });
 });
+
+
+
+    var regExCusID = /^[C]{1}[0-9]{0,50}$/;
+
+    $("#txtCusID").keyup(function () {
+    let input = $("#txtCusID").val();
+    if(regExCusID.test(input)) {
+        $("#txtCusID").css('border','2px solid green');
+        $("#error").text("");
+    } else {
+        $("#txtCusID").css('border','2px solid red');
+        $("#error").text("This is a Wrong format : C001");
+    }
+    });
+
+    var regExCusNAME = /^[A-z]{3,20}$/;
+
+    $("#txtName").keyup(function () {
+     let  input = $("#txtName").val();
+     if (regExCusNAME.test(input)) {
+         $("#txtName").css('border','2px solid green');
+         $("#error").text("");
+
+     } else {
+         $("#txtName").css('border' , '2px solid red');
+         $("#error").text("This is Wrong format : 1234");
+     }
+    });
+
