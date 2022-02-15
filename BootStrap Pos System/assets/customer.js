1a4c19001350464d7c2036b6c5b92bb949a350d9
Mish880 <Pereraittec6@gmail.com>
2022-02-15 14:52:52 +0530

    Update BootStrap

--- BootStrap Pos System/assets/customer.js
+++ BootStrap Pos System/assets/customer.js
@@ -1,104 +1,217 @@
-/*Add Customer Script*/
-
-/*Button Selection*/
+/*This is Startup of the event*/
 $("#btnSaveCustomer").click(function () {
+     saveCustomer();
+     clearAll();
+     loadAllCustomers();
+});
 
-    /*remove all the row click events added before*/
-    $("#cusTable>tr").off("click");
+ /*Search Customer*/
+$("#btnSearch").click(function () {
+   var searchID = $("#txtSearchCusID").val();
 
-    /*gather the customer information*/
+   var response = searchCustomer(searchID);
+   if(response) {
+       $("#txtCusID").val(response.id);
+       $("#txtName").val(response.name);
+       $("#txtAddress").val(response.address);
+       $("#txtContact").val(response.contact);
+   }else{
+       clearAll();
+       alert("No  Such a Customer");
+   }
+});
+  /*The End of the Event*/
+   /*CRUD OPERATIONS IS START*/
+  function loadAllCustomers() {
+      $("#cusTable").empty();
+         for (var i of  customerDB) {  /*Java Script's has a loop for is loop of java Script*/
+           /*First create a html row*/
+          let row = `<tr><td>${i.id}</td><td>${i.name}</td><td>${i.address}</td><td>${i.contact}</td></tr>`;
+           /*Select the table body and append the row*/
+          $("#cusTable").append(row);
+      }
+    }
+function saveCustomer() {
+    /*First gather the customer information*/
     let customerID = $("#txtCusID").val();
     let customerName = $("#txtName").val();
     let customerAddress = $("#txtAddress").val();
     let customerContact = $("#txtContact").val();
 
-    /*Create a html path*/
-    let row = `<tr><td>${customerID}</td><td>${customerName}</td><td>${customerAddress}</td><td>${customerContact}</td></tr>>`;
+    /*Create a Object*/
+    var customerObject = {
+        id : customerID,
+        name : customerName,
+        address : customerAddress,
+        contact : customerContact
+    };
+    customerDB.push(customerObject);
+}
 
-    /*Select the table append to the row*/
-    $("#cusTable").append(row);
+function searchCustomer(id) {
+      for (let i = 0; i < customerDB.length; i++) {
+          if(customerDB[i].id == id) {
+              return customerDB[i];
+          }
+      }
+  }
 
-    /*bind the event after the row*/
-    $("#cusTable>tr").click(function () {
-        /*this*/ /*dom object*/
-        /*$(this); This is a JQuery Object*/
+  function deleteCustomer() {
 
-        /*$(this)*/ /*tr*/
-        /*$(this).children();*/ /*Return all td inside selected row*/
+  }
+  function updateCustomer() {
 
-        let cusID = $(this).children(":eq(0)").text(); /*Select the first td and get the text*/
-        let cusName = $(this).children(":eq(1)").text();
-        let cusAddress = $(this).children(":eq(2)").text();
-        let cusContact = $(this).children(":eq(3)").text();
+  }
 
-        console.log(cusID, cusName, cusAddress, cusContact);
+  /*CRUD Operation ENDED*/
 
-        /*Set values to the input fields*/
+   /*Validation Strated (Regular Expressions)*/
+   const cusIDRegEx = /^[C]{1}[0-9]{0,50}$/;
+   const cusNameRegEx = /^[A-z]{3,20}$/;
+   const cusAddressRegEx = /^[A-z]{3,20}$/;
+   const cusContactRegEx = /^[1-9][0-9]*[0-9]{2}?$/;
 
-        $("#txtCusID").val(cusID);
-        $("#txtName").val(cusName);
-        $("#txtAddress").val(cusAddress);
-        $("#txtContact").val(cusContact);
-    });
-
-
-    /*Remove selected row form the double click*/
-    $("#cusTable>tr").dblclick(function () {
-        $(this).remove();
-    });
-});
-
-
-
-    var regExCusID = /^[C]{1}[0-9]{0,50}$/;
-
-    $("#txtCusID").keyup(function () {
-    let input = $("#txtCusID").val();
-    if(regExCusID.test(input)) {
-        $("#txtCusID").css('border','2px solid green');
-        $("#error").text("");
-    } else {
-        $("#txtCusID").css('border','2px solid red');
-        $("#error").text("This is a Wrong format : C001");
-    }
-    });
-
-    var regExCusNAME = /^[A-z]{3,20}$/;
-
-    $("#txtName").keyup(function () {
-     let  input = $("#txtName").val();
-     if (regExCusNAME.test(input)) {
-         $("#txtName").css('border','2px solid green');
-         $("#error").text("");
-
-     } else {
-         $("#txtName").css('border' , '2px solid red');
-         $("#error").text("This is Wrong format : 1234");
-     }
-    });
-
-    var regExCUSADDRESS = /^[A-z]{3,20}$/;
-
-    $("#txtAddress").keyup(function () {
-        let input = $("#txtAddress").val();
-        if (regExCUSADDRESS.test(input)) {
-            $("#txtAddress").css('border' , '2px solid green');
-            $("#error").text("");
-        } else {
-            $("#txtAddress").css('border', '2px solid red');
-            $("#error").text("This is Wrong format :Colombo - 1----2---");
-        }
-    });
-
-    var regExCUSCONTACT = /^[1-9][0-9]*[0-9]{2}?$/;
-
-    $("#txtContact").keyup(function () {
-       let  input = $("#txtContact").val();
-       if(regExCUSCONTACT.test(input)) {
-          $("#txtContact").css('border' , '2px solid green');
-          $("#error").text("");
-       } else {
-           $("#txtContact").css('border' , '2px solid red');
-           $("#error").text("This is wrong format :qwer");
+   $('#txtCusID,#txtName,#txtAddress,#txtContact').on('keydown' , function (eventOb) {
+       if (eventOb.key == "Tab") {
+           eventOb.preventDefault(); /*This is a Stop execution of the button*/
        }
-    });
+   });
+
+   $('#txtCusID,#txtName,#txtAddress,#txtContact').on('blur',function () {
+      fromVaild();
+   });
+
+   $("#txtCusID").on('keyup',function (eventOb) {
+    setButton();
+    if (eventOb.key == "Enter") {
+        checkIfVaild();
+    }
+    if (eventOb.key == "Control") {
+        var typeCustomerID = $("#txtCusID").val();
+        var srcCustomer = searchCustomerFromID(typeCustomerID);
+        $("#txtCusID").val(srcCustomer.getCustomerID());
+        $("#txtName").val(srcCustomer.getCustomerName());
+        $("#txtAddress").val(srcCustomer.getCustomerAddress());
+        $("#txtContact").val(srcCustomer.getCustomerContact());
+    }
+
+   });
+
+   $("#txtName").on('keyup', function (eventOb) {
+     setButton();
+     if (eventOb.key == "Enter") {
+         checkIfVaild();
+     }
+   });
+
+   $("#txtAddress").on('keyup' , function (eventOb) {
+      setButton();
+      if (eventOb.key == "Enter") {
+          checkIfVaild();
+      }
+   });
+
+   $("#txtContact").on('keyup' , function (eventOb) {
+      setButton();
+      if (eventOb.key == "Enter") {
+          checkIfVaild();
+      }
+   });
+   /*Focuing the events end*/
+   $("#btnSaveCustomer").attr('disabled',true);
+
+   function clearAll() {
+       $('#txtCusID,#txtName,#txtAddress,#txtContact').val("");
+       $('#txtCusID,#txtName,#txtAddress,#txtContact').css('border','2px solid #ced4da');
+       $('#txtCusID').focus();
+       $("#btnSaveCustomer").attr('disabled', true);
+       loadAllCustomers();
+       $("#lblcusid,#lblcusname,#lblcusaddress,#lblcuscontact").text("");
+   }
+
+   function fromVaild() {
+       var cusId = $("#txtCusID").val();
+       $("#txtCusID").css('border' , '2px Solid green');
+       $("#lblcusid").text("");
+       if(cusIDRegEx.test(cusId)) {
+           var cusName = $("#txtName").val();
+           if (cusNameRegEx.test(cusName)) {
+               $("#txtName").css('border', '2px solid green');
+               $("#lblcusname").text("");
+               var cusAddress = $("#txtAddress").val();
+               if(cusAddressRegEx.test(cusAddress)) {
+                   var cusContact  = $("#txtContact").val();
+                   var resp = cusContactRegEx.test(cusContact);
+                   $("#txtAddress").css('border' , '2px solid green');
+                   $("#lblcusaddress").text("");
+                   if (resp) {
+                       $("#txtContact").css('border' , '2px solid green');
+                       $("#lblcuscontact").text("");
+                       return true;
+                   } else {
+                       $("#txtContact").css('border' , '2px solid red');
+                       $("#lblcuscontact").text("Your Contact is reqired field : pattern +9412345 or 200");
+                       return false;
+                   }
+               } else {
+                   $("#txtAddress").css('border' , '2px solid red');
+                   $("#lblcusaddress").text("Your Address is a required field : Mimum 7");
+                   return false;
+               }
+           } else {
+               $("#txtName").css('border', '2px solid red');
+               $("#lblcusname").text("Your Name is a required field : Mimimum 5 , Max 20 , Spaces Allowed");
+               return false;
+           }
+       } else {
+           $("#txtCusID").css('border' , '2px solid red');
+           $("#lblcusid").text("Your ID is a required field : Pattern C001");
+           return false;
+       }
+   }
+
+   function checkIfVaild() {
+       var cusID = $("#txtCusID").val();
+       if(cusIDRegEx.test(cusID)) {
+           $("#txtName").focus();
+           var cusName = $("#txtName").val();
+           if(cusNameRegEx.test(cusName)) {
+               $("#txtAddress").focus();
+               var cusAddress = $("#txtAddress").val();
+               if (cusAddressRegEx.test(cusAddress)) {
+                   $("#txtContact").focus();
+                   var cusContact = $("#txtContact").val();
+                   var resp  = cusContactRegEx.test(cusContact);
+                   if(resp) {
+                       let res = confirm("Do you want to add this Customer...?");
+                       if(res) {
+                           saveCustomer();
+                           clearAll();
+                         }
+                       } else {
+                           $("#txtContact").focus();
+                       }
+
+                   } else {
+                       $("#txtAddress").focus();
+                   }
+
+               } else {
+                   $("#txtName").focus();
+               }
+           } else {
+               $("#txtCusID").focus();
+           }
+       }
+
+       function setButton() {
+           let b = fromVaild();
+           if (b) {
+               $("#btnSaveCustomer").attr('disabled',false);
+           } else {
+               $("#btnSaveCustomer").attr('disabled',true);
+           }
+       }
+      $('#btnSaveCustomer').click(function () {
+          checkIfVaild();
+      });
